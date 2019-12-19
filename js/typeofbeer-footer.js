206479bd35c3144eaeb5a15712340695d85a5619
lolthoi <manchestervn1996@gmail.com>
2019-12-19 15:22:20 +0700

    demo-lan-22 (added nutrition, type and how to brew, culture, fix header--- transformed footer into js, rename some title)

---
+++ js/typeofbeer-footer.js
@@ -0,0 +1,53 @@
+$("body").append(()=>{
+    return " <footer class=\"footer-bs\">\n" +
+        "        <div class=\"row\">\n" +
+        "            <div class=\"col-md-3 footer-brand animated fadeInLeft\">\n" +
+        "                <img  alt src=\"img/logo.png\" style=\"height: 69px\">\n" +
+        "                <p>We are team Beer Guys, we create amazing websites for beer lovers.</p>\n" +
+        "                <p>© 2019 Beer Guys Copyright, All rights reserved</p>\n" +
+        "            </div>\n" +
+        "            <div class=\"col-md-4 footer-nav animated fadeInUp\">\n" +
+        "                <h4>Menu —</h4>\n" +
+        "                <div class=\"col-md-6\">\n" +
+        "                    <ul class=\"pages\">\n" +
+        "                        <li><a href=\"#\">Travel</a></li>\n" +
+        "                        <li><a href=\"#\">Nature</a></li>\n" +
+        "                        <li><a href=\"#\">Explores</a></li>\n" +
+        "                        <li><a href=\"#\">Science</a></li>\n" +
+        "                        <li><a href=\"#\">Advice</a></li>\n" +
+        "                    </ul>\n" +
+        "                </div>\n" +
+        "                <div class=\"col-md-6\">\n" +
+        "                    <ul class=\"list\">\n" +
+        "                        <li><a href=\"#\">About Us</a></li>\n" +
+        "                        <li><a href=\"#\">Contacts</a></li>\n" +
+        "                        <li><a href=\"#\">Terms & Condition</a></li>\n" +
+        "                        <li><a href=\"#\">Privacy Policy</a></li>\n" +
+        "                    </ul>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "            <div class=\"col-md-2 footer-social animated fadeInDown\">\n" +
+        "                <h4>Follow Us</h4>\n" +
+        "                <ul>\n" +
+        "                    <li><a href=\"#\">Facebook</a></li>\n" +
+        "                    <li><a href=\"#\">Twitter</a></li>\n" +
+        "                    <li><a href=\"#\">Instagram</a></li>\n" +
+        "                    <li><a href=\"#\">RSS</a></li>\n" +
+        "                </ul>\n" +
+        "            </div>\n" +
+        "            <div class=\"col-md-3 footer-ns animated fadeInRight\">\n" +
+        "                <h4>Newsletter</h4>\n" +
+        "                <p>A rover wearing a fuzzy suit doesn’t alarm the real penguins</p>\n" +
+        "                <p>\n" +
+        "                <div class=\"input-group\">\n" +
+        "                    <input type=\"text\" class=\"form-control\" placeholder=\"Search for...\">\n" +
+        "                    <span class=\"input-group-btn\">\n" +
+        "                        <button class=\"btn btn-default\" type=\"button\"><span class=\"glyphicon glyphicon-envelope\"></span></button>\n" +
+        "                      </span>\n" +
+        "                </div><!-- /input-group -->\n" +
+        "                </p>\n" +
+        "            </div>\n" +
+        "        </div>\n" +
+        "    </footer>\n" +
+        "    <section style=\"text-align:center; margin:10px auto;\"><p>Designed by <a href=\"http://beer-guys.herokuapp.com/\">Team Beer Guys</a></p></section>"
+});
